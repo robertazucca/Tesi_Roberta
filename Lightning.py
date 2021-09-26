73f96cbfc18d2e66857bf71c51dc4a42fd676bd1
roberta <roberta.zucca57@gmail.com>
2021-09-26 23:37:22 +0200

    add

--- Lightning.py
+++ Lightning.py
@@ -11,18 +11,17 @@ class LightningNetwork:
 
         df_data = pd.json_normalize(data)
 
-        nodes_set = set()
-
-        n=0
-        for i in range(len(df_data)):
-            if(df_data.loc[i,'node1_policy.min_htlc']) == None and df_data.loc[i,'node2_policy.min_htlc'] == None :
-                n = n+1
-
-        print("Percentuale canali in cui non conosco la policy: ", str(round(n*100/len(df_data),2)))
+        #per calcolare la percentuale dei canali in cui non conosco le policy di inoltro
+        #n=0
+        #for i in range(len(df_data)):
+        #    if(df_data.loc[i,'node1_policy.min_htlc']) == None and df_data.loc[i,'node2_policy.min_htlc'] == None :
+        #        n = n+1
+        #print("Percentuale canali in cui non conosco la policy: ", str(round(n*100/len(df_data),2)))
 
         
         self.G = nx.MultiGraph(data = True)
 
+        #ispeziono ogni canale, se i campi delle policy hanno valore null, setto il valore del campo a -1
         for i in range(len(df_data)):
             
             self.G.add_edge(df_data.loc[i,'node1_pub'],df_data.loc[i,'node2_pub'],df_data.loc[i,'channel_id'], 
@@ -33,34 +32,11 @@ class LightningNetwork:
             FEERATE1 = -1 if (df_data.loc[i,'node1_policy.fee_rate_milli_msat']) == None else int(df_data.loc[i,'node1_policy.fee_rate_milli_msat']),
             FEERATE2 = -1 if (df_data.loc[i,'node2_policy.fee_rate_milli_msat']) == None else int(df_data.loc[i,'node2_policy.fee_rate_milli_msat'])
             )
-        #elif mode == 'analisi_graph':
-         #   for i in range(len(df_data)):
-                #edges.append((df_data.loc[i,'node1_pub'],df_data.loc[i,'node2_pub'],df_data.loc[i,'channel_id'],{
-         #       self.G.add_edge(df_data.loc[i,'node1_pub'],df_data.loc[i,'node2_pub'],df_data.loc[i,'channel_id']) #{
-                    #'MINHTLC_1' : 0 if (df_data.loc[i,'node1_policy.min_htlc']) == None else int(df_data.loc[i,'node1_policy.min_htlc']),
-                    #'MINHTLC_2' : 0 if (df_data.loc[i,'node2_policy.min_htlc']) == None else int(df_data.loc[i,'node2_policy.min_htlc']),
-                    #'FEEBASE_1' : 0 if (df_data.loc[i,'node1_policy.fee_base_msat']) == None else int(df_data.loc[i,'node1_policy.fee_base_msat']),
-                    #'FEEBASE_2' : 0 if (df_data.loc[i,'node2_policy.fee_base_msat']) == None else int(df_data.loc[i,'node2_policy.fee_base_msat']),
-                    #'FEERATE_1' : 0 if (df_data.loc[i,'node1_policy.fee_rate_milli_msat']) == None else int(df_data.loc[i,'node1_policy.fee_rate_milli_msat']),
-                    #'FEERATE_2' : 0 if (df_data.loc[i,'node2_policy.fee_rate_milli_msat']) == None else int(df_data.loc[i,'node2_policy.fee_rate_milli_msat'])
-                #})
 
 
         for edge in self.G.edges():
-          #self.G.add_node(edge[0])
-          #self.G.add_node(edge[1])
-
-            nodes_set.add(edge[0])
-            nodes_set.add(edge[1])
-
-        nodes = list(nodes_set)
-
-        
-
-
-        #self.G = nx.MultiGraph(data = True)
-        self.G.add_nodes_from(nodes)
-        #self.G.add_edgesllll_from(edges)
+          self.G.add_node(edge[0])
+          self.G.add_node(edge[1])
 
 
     def crea_grafo(self,name):
