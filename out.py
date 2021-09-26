7f926ea5ed46452bd8b851000fc0156b88cb7674
roberta <roberta.zucca57@gmail.com>
2021-09-26 23:37:31 +0200

    add

--- out.py
+++ out.py
@@ -3,11 +3,7 @@ import Lightning as LN
 channels_filename = 'edges.json'
 nodes_filename = 'nodes.json'
 
-#grafo.gml per analisi
-#ln=LN.LightningNetwork(channels_filename, nodes_filename,mode='analisi_graph')
-#ln.crea_grafo("grafo.gml")
-
-#graphia.gml per visualizzazione con graphia
-ln1=LN.LightningNetwork(channels_filename, nodes_filename) 
-ln1.crea_grafo("grafodef.gml")
+#disegno grafo per visualizzazione con graphia
+ln=LN.LightningNetwork(channels_filename, nodes_filename) 
+ln.crea_grafo("grafodef.gml")
 
